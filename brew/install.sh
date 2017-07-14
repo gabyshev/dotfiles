3771058342dedf42d39136f12530a875eb46bfda
Simon Gabyshev <semen.gabyshev@lazada.com>
2017-07-14 14:58:00 +0700

    homebrew

---
+++ brew/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo -v
+
+if test ! $(which brew)
+then
+    echo "Installing Homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew update
+brew upgrade
+
+apps=(
+  git
+  zsh
+  zsh-syntax-highlighting
+  tree
+)
+
+brew install "${apps[@]}"
+
+brew cleanup
